3426dff5335774a2221a0e4b602438d4416d1faf
Prakhar <mprakhar07@gmail.com>
2025-09-30 20:27:30 +0530

    learning about class based component vs functional components

--- foodapp/components/About.js
+++ foodapp/components/About.js
@@ -4,6 +4,9 @@ const About = () => {
   return (
     <div>
       <h1>hello</h1>
+      <h1>Prakhar Mishra</h1>
+      <h2>Lucknow,Up</h2>
+      <h3>prakharcodes@xyz</h3>
     </div>
   );
 };
---
+++ foodapp/components/UserClass.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class userClass extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>name</h1>
+        <h2>place</h2>
+        <div>contact</div>
+      </div>
+    );
+  }
+}
