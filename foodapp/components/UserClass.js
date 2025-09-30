045c401b3fae1bf589f4a049c55856f7512bf24c
Prakhar <mprakhar07@gmail.com>
2025-09-30 22:24:35 +0530

    creation of state variables in class components

--- foodapp/components/UserClass.js
+++ foodapp/components/UserClass.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 class userClass extends React.Component {
+  //setting a state in the class components
+  //create a constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      count1: 2,
+    }; //global state variable for the components// all these states will be created here itself //
+  }
+
   render() {
+    const count1 = this.state.count;
+    const count2 = this.state.count1; //now  we have state variables//
     return (
       <div>
-        <h1>name</h1>
-        <h2>place</h2>
-        <div>contact</div>
+        <h1>Count1 is : {count1}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        ></button>
       </div>
     );
   }
