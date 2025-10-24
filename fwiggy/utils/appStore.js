0dc71a9ec2624bc7e9f905c318514591e60a90f4
Prakhar <mprakhar07@gmail.com>
2025-10-24 18:53:50 +0530

    configure the cart store

--- fwiggy/utils/appStore.js
+++ fwiggy/utils/appStore.js
@@ -1,5 +1,10 @@
 import configureStore from "./configureStore";
+import cartReducer from "../examples/redux/README.md";
 
-const appStore = configureStore({});
+const appStore = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },  
+});
 
 export default  appStore;
